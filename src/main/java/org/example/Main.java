1d9c1039201c0a8b68009e99adaee48502f22ed0
webermuros1 <webermuros1@gmail.com>
2026-03-03 13:22:40 +0100

    change ids personal

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,18 +9,18 @@ public class Main {
     public static void main(String[] args) {
 
         // Patients
-        ConsultationPatient p1 = new ConsultationPatient("Ana", 25, "P1", "Flu");
-        ConsultationPatient p2 = new ConsultationPatient("Luis", 30, "P2", "Headache");
+        ConsultationPatient p1 = new ConsultationPatient("Ana", 25, "123456", "Flu");
+        ConsultationPatient p2 = new ConsultationPatient("Luis", 30, "6321", "Headache");
 
-        AmbulancePatient p3 = new AmbulancePatient("Carlos", 40, "P3", "Accident");
-        AmbulancePatient p4 = new AmbulancePatient("Maria", 50, "P4", "Heart attack");
+        AmbulancePatient p3 = new AmbulancePatient("Carlos", 40, "58926", "Accident");
+        AmbulancePatient p4 = new AmbulancePatient("Maria", 50, "1452", "Heart attack");
 
         // Doctors
-        Specialist d1 = new Specialist("Dr. House", 45, "D1", 40, "Diagnostics");
-        Specialist d2 = new Specialist("Dr. Strange", 50, "D2", 35, "Surgery");
+        Specialist d1 = new Specialist("Dr. House", 45, "96328", 40, "Diagnostics");
+        Specialist d2 = new Specialist("Dr. Strange", 50, "521748", 35, "Surgery");
 
-        StudentDoctor d3 = new StudentDoctor("John", 28, "D3", 20, 15, "Cardiology");
-        StudentDoctor d4 = new StudentDoctor("Laura", 26, "D4", 25, 20, "Neurology");
+        StudentDoctor d3 = new StudentDoctor("John", 28, "201368", 20, 15, "Cardiology");
+        StudentDoctor d4 = new StudentDoctor("Laura", 26, "96584", 25, 20, "Neurology");
 
         // Print patients
         System.out.println("=== PATIENTS ===");
