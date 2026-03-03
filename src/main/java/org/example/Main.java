87b6745259ae82be6634afa1ff8d95f47f092de8
webermuros1 <webermuros1@gmail.com>
2026-03-03 14:14:31 +0100

    change id for instand

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,28 +8,27 @@ import org.example.patients.ConsultationPatient;
 public class Main {
     public static void main(String[] args) {
 
-        // Patients
+
         ConsultationPatient p1 = new ConsultationPatient("Ana", 25, "123456", "Flu");
         ConsultationPatient p2 = new ConsultationPatient("Luis", 30, "6321", "Headache");
 
         AmbulancePatient p3 = new AmbulancePatient("Carlos", 40, "58926", "Accident");
         AmbulancePatient p4 = new AmbulancePatient("Maria", 50, "1452", "Heart attack");
 
-        // Doctors
+
         Specialist d1 = new Specialist("Dr. House", 45, "96328", 40, "Diagnostics");
         Specialist d2 = new Specialist("Dr. Strange", 50, "521748", 35, "Surgery");
 
         StudentDoctor d3 = new StudentDoctor("John", 28, "201368", 20, 15, "Cardiology");
         StudentDoctor d4 = new StudentDoctor("Laura", 26, "96584", 25, 20, "Neurology");
 
-        // Print patients
-        System.out.println("=== PATIENTS ===");
+                System.out.println("=== PATIENTS ===");
         p1.showInfo();
         p2.showInfo();
         p3.showInfo();
         p4.showInfo();
 
-        // Print doctors
+
         System.out.println("\n=== DOCTORS ===");
         d1.showInfo();
         d2.showInfo();
